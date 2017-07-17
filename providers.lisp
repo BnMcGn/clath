43681757cc8d98ed56288917f04fcff4c6d945bb
bnmcgn@gmail.com <bnmcgn@gmail.com>
2017-07-17 06:56:42 -0700

    Added initial data for github signin.

--- providers.lisp
+++ providers.lisp
@@ -5,7 +5,8 @@
       ((err (assoc :error parameters))
        (err-msg (assoc :error--description parameters)))
     (error
-     (format nil "OpenID Server Error: ~a, ~a" (cdr err) (cdr err-msg)))))
+     (format nil "OpenID Server Error: ~a, ~a" (cdr err) (cdr err-msg))))
+  parameters)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defun default-token-processor (parameters)
@@ -35,12 +36,20 @@
      :endpoints-url
      "https://accounts.google.com/.well-known/openid-configuration"
      :auth-scope "openid profile email"
-     :token-processor ,(function google-token-processor))))
+     :token-processor ,(function google-token-processor))
+    :github
+    (:string "github"
+     :auth-endpoint "https://github.com/login/oauth/authorize"
+     :token-endpoint "https://github.com/login/oauth/access_token"
+     :userinfo-endpoint "https://api.github.com/user"
+     ;;FIXME: email endpoint not implemented yet.
+     :email-endpoint "https://api.github.com/user/emails"
+     :auth-scope ""
+     :token-processor ,(function default-token-processor))))
 
 (defparameter *provider-secrets* nil)
 
 ;;;Token processors, given the callback parameters as an alist, should return, as values,
 ;;; the access token, and the id token. Providers vary about what this means. Facebook,
 ;;; for example, uses the access-token as the id-token, whereas Google does something else.
-
 
